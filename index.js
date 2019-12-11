c9bc63ac1453cd2ced43d8ad678e74052e4241d2
Viktor <meow.lululu@gmail.com>
2019-12-11 15:37:40 +0200

    FrogJmp

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // const selectionSort = require('./src/SelectionSort');
 // const sum = require('./src/recursion');
 // require('./src/QuickSort');
-// require('./src/playground');
+require('./src/playground');
 // require('./src/Codility/array_1');
 // require('./src/Codility/Arrays/array_2');
 require('./src/Codility/Time Complexity/FrogJmp');
--- src/playground.js
+++ src/playground.js
@@ -1,21 +1,32 @@
-function solution(N) {
-  const binary = N.toString(2).split('');
+// function solution(N) {
+//   const binary = N.toString(2).split('');
 
-  let gaps = [];
-  let gap = 0;
+//   let gaps = [];
+//   let gap = 0;
 
-  binary.forEach(value => {
-    if (value == 1) {
-      gaps.push(gap);
-      gap = 0;
-    }
+//   binary.forEach(value => {
+//     if (value == 1) {
+//       gaps.push(gap);
+//       gap = 0;
+//     }
 
-    if (value == 0) {
-      return gap++;
-    }
-  });
+//     if (value == 0) {
+//       return gap++;
+//     }
+//   });
 
-  return Math.max.apply(Math, gaps);
-}
+//   return Math.max.apply(Math, gaps);
+// }
 
-solution(529);
+// solution(529);
+
+var a = {},
+  b = { key: 'b' },
+  c = { key: 'c' },
+  d = { key: 'd' };
+
+a[b] = 123;
+a[c] = 456;
+a[d] = 666;
+
+// console.log(a[b]);
